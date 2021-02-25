50551d4a75032a7653f43b2daacb7475e384c4d9
Aron Wiley <awiley.dev@gmail.com>
2021-02-24 18:01:37 -0600

    Some changes

--- BoundProperties.cs
+++ BoundProperties.cs
@@ -1,4 +1,27 @@
-$HEADER$namespace $NAMESPACE$
-{
-  public class $CLASS$ {$END$}
+using System;
+using System.ComponentModel;
+using JetBrains.Annotations;
+
+namespace SerialTest {
+    public class BoundProperties : INotifyPropertyChanged {
+        private string _Response;
+        [NotNull]
+        public string Response {
+            get => _Response;
+            set {
+                _Response = value;
+                OnPropertyChanged(nameof(Response));
+            }
+        }
+
+        public BoundProperties() {
+            _Response = "";
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
 }
--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,11 +1,28 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+
+//Serial communication
+using System.IO.Ports;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace SerialTest {
     public class MainWindow : Window {
+        #region Global Variables
+        private static SerialPort _serialPort = new SerialPort();
+        private static Socket _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private readonly BoundProperties _boundProperties = new BoundProperties();
+        #endregion
+        
         public MainWindow() {
             InitializeComponent();
+            DataContext = _boundProperties;
+            
             #if DEBUG
             this.AttachDevTools();
             #endif
@@ -13,6 +30,10 @@ namespace SerialTest {
 
         private void InitializeComponent() {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void StartClick(object sender, RoutedEventArgs e) {
+            
         }
     }
 }
